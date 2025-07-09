de414a8fc47a2eec5365d5487731ec83cca663fb
yunus-emre-yaman <yunusemreyaman359@gmail.com>
2025-07-09 16:01:28 +0300

    productlist.jsx kodları düzenlendi

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,4 +1,6 @@
-import Product from "./../components/Product";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Product from "./Product";
 
 /*
   Bu component;
@@ -10,11 +12,22 @@ import Product from "./../components/Product";
   NOT: Map fonksiyonu ile listeleme görevi diğer testleri etkiliyor olabilir. Buna göre, listeleme görevini tamamladıktan sonra birkaç test birden geçebilir.
 */
 
-export default function ProductList(props) {
+export default function ProductList({ activeCategory }) {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    if (!activeCategory) return;
+
+    axios
+      .get(`https://fakestoreapi.com/products/category/${activeCategory}`)
+      .then((res) => setProducts(res.data))
+      .catch(() => setProducts([]));
+  }, [activeCategory]);
+
   return (
     <main className="productList">
-      <h2 data-testid="productList-title">{/* active category */} ürünleri</h2>
-      {[].map((item) => (
+      <h2 data-testid="productList-title">{activeCategory} ürünleri</h2>
+      {products.map((item) => (
         <Product key={item.id} product={item} />
       ))}
     </main>
